6c52e24b2850acc0a9225362468e849cf0b10810
Barış Kartal <kartalbaris@gmail.com>
2017-11-01 23:27:13 +0200

    Added validation for duplicate user share.

---
+++ app/Http/Requests/API/ShareRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\API;
+
+use Illuminate\Validation\Rule;
+use App\Http\Requests\Request;
+
+class ShareRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'portfolio_id' => 'required|integer',
+            'symbol_id' => ['required', 'integer',
+                            Rule::unique('shares')->where(function ($query) {
+                                return $query->where('portfolio_id', $this->portfolio_id)
+                                             ->where('symbol_id', $this->symbol_id);
+                            })
+            ]
+        ];
+    }
+}
