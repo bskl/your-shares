e84bdb6b6cd28270c6eeb203ea5561e08d042feb
Savaş KARTAL <savaskartal@gmail.com>
2018-01-16 15:00:15 +0300

    added money attribute default value for creating portfolio

--- app/Http/Controllers/API/PortfolioController.php
+++ app/Http/Controllers/API/PortfolioController.php
@@ -25,6 +25,7 @@ class PortfolioController extends Controller
         $data['order'] = ++$order;
 
         $portfolio = Portfolio::create($data);
+        $portfolio->total_amount = $portfolio->total_average_amount = $portfolio->total_gain = '0';
         $portfolio->load('shares');
 
         return response()->json($portfolio);
