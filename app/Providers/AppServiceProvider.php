2347acaf4ba27dd3b55e969a47f76ff8bfd13655
Barış Kartal <bskl@Kartal-MacBook-Pro.local>
2018-03-11 23:52:00 +0300

    Delete comment lines

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,9 +13,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //\Horizon::auth(function ($request) {
-            //dd(auth()->user());
-        //});
     }
 
     /**
