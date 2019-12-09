9c28931a41255d56c3a24532ebbf4c6363ceb274
Barış Kartal <kartalbaris@gmail.com>
2019-12-09 21:01:53 +0300

    Fix return error message

--- app/Http/Controllers/API/Auth/ResetPasswordController.php
+++ app/Http/Controllers/API/Auth/ResetPasswordController.php
@@ -62,7 +62,7 @@ class ResetPasswordController extends Controller
     protected function sendResetFailedResponse(Request $request, $response)
     {
         return response()->json(
-            ['messages' => '', 'errors' => [['email' => trans($response)]]],
+            ['messages' => '', 'errors' => ['email' => [trans($response)]]],
             Response::HTTP_UNPROCESSABLE_ENTITY
         );
     }
