28c4437c48455073163ae36316227b8464824743
bskl <kartalbaris@gmail.com>
2020-04-04 14:27:38 +0000

    Apply fixes from StyleCI

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -30,10 +30,10 @@ class Handler extends ExceptionHandler
      * Report or log an exception.
      *
      * @param \Throwable $exception
-     * @return void
-     *
      *
      * @throws \Throwable
+     *
+     * @return void
      */
     public function report(Throwable $exception)
     {
@@ -43,11 +43,12 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request $request
-     * @param  \Throwable               $exception
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @param \Illuminate\Http\Request $request
+     * @param \Throwable               $exception
      *
      * @throws \Throwable
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function render($request, Throwable $exception)
     {
--- config/mail.php
+++ config/mail.php
@@ -63,7 +63,7 @@ return [
 
         'log' => [
             'transport' => 'log',
-            'channel' => env('MAIL_LOG_CHANNEL'),
+            'channel'   => env('MAIL_LOG_CHANNEL'),
         ],
 
         'array' => [
