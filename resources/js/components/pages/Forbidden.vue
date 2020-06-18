d9db8d3f8f5383788855c812177d07e1737e5f51
Barış Kartal <kartalbaris@gmail.com>
2020-06-18 19:59:49 +0300

    Fix message.

--- resources/js/components/pages/Forbidden.vue
+++ resources/js/components/pages/Forbidden.vue
@@ -2,7 +2,7 @@
   <v-layout align-center>
     <v-flex text-center>
       <h1 class="display-4 red--text text--lighten-2">403</h1>
-      <p class="red--text text--lighten-2">{{ $t("Page not found.") }}</p>
+      <p class="red--text text--lighten-2">{{ $t("This action is unauthorized.") }}</p>
       <v-btn to="/" class="btn-action">
         {{ $t("Return Home Page") }}
       </v-btn>
