ea6c47353d2d50ca38398b3d52b2fb9313d0e624
Barış Kartal <kartalbaris@gmail.com>
2020-08-28 23:29:32 +0300

    Delete unused variable.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,15 +8,6 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        Commands\SetSymbols::class,
-    ];
-
-    /**
      * Define the application's command schedule.
      *
      * @param \Illuminate\Console\Scheduling\Schedule $schedule
