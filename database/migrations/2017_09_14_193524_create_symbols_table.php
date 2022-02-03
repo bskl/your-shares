3f6eeba151e6d3f216bdf7269b748044b406ec2c
Barış Kartal <kartalbaris@gmail.com>
2022-02-04 02:51:15 +0300

    Update

--- database/migrations/2017_09_14_193524_create_symbols_table.php
+++ database/migrations/2017_09_14_193524_create_symbols_table.php
@@ -20,7 +20,7 @@ class CreateSymbolsTable extends Migration
             $table->string('title')->nullable();
             $table->tinyInteger('trend');
             $table->integer('last_price');
-            $table->string('rate_of_change', 5);
+            $table->string('rate_of_change', 10);
             $table->timestamp('session_time')->useCurrent();
             $table->timestamps();
         });
